a745aa304ffd41e70713d6070e8d6e7b2545d210
SpacePiercer <101861551+SpacePiercer@users.noreply.github.com>
2026-01-31 13:50:48 -0800

    wip

--- code.R
+++ code.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
-sample_submission<-read_csv("~/Documents/Uni Related/HackML26/sample_submission.csv")
-test_set<-read_csv("~/Documents/Uni Related/HackML26/test.csv")
-train_set<-read_csv("~/Documents/Uni Related/HackML26/train.csv")
+sample_submission<-read_csv("C:\\Users\\georg\\OneDrive\\Desktop\\HackML2026\\sample_submission.csv")
+test_set<-read_csv("C:\\Users\\georg\\OneDrive\\Desktop\\HackML2026\\test.csv")
+train_set<-read_csv("C:\\Users\\georg\\OneDrive\\Desktop\\HackML2026\\train.csv")
+
+head(test)
