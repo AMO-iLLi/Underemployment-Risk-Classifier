1a725192e16f70a59488763b4a46cf0d1abc3b42
Ilia jn <153955608+AMO-iLLi@users.noreply.github.com>
2026-01-31 11:18:29 -0800

    Getting Started WIP

---
+++ code.R
@@ -0,0 +1,3 @@
+sample_submission<-read_csv("~/Documents/Uni Related/HackML26/sample_submission.csv")
+test_set<-read_csv("~/Documents/Uni Related/HackML26/test.csv")
+train_set<-read_csv("~/Documents/Uni Related/HackML26/train.csv")
